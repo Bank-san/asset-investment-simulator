44a5e2ae2fda223cfefa6ffa41b3a3e3591f6756
bank-san <taichi.a@itnav.co.jp>
2023-06-12 08:53:59 +0900

    元本、増加額追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 
 export default defineComponent({
   name: 'AssetSimulator',
@@ -7,27 +7,30 @@ export default defineComponent({
     const investmentPeriod = ref(10);
     const monthlyInvestment = ref(10000);
     const annualInterestRate = ref(5);
-    const assetValue = ref<number | null>(null);
-    
-    const calculateAssetValue = () => {
+    const assetValue = ref(calculateAssetValue());
+
+    function calculateAssetValue() {
       const totalMonths = investmentPeriod.value * 12;
       const monthlyInterestRate = annualInterestRate.value / 100 / 12;
-      
+
       let currentValue = 0;
       for (let i = 0; i < totalMonths; i++) {
         currentValue += monthlyInvestment.value;
         currentValue *= 1 + monthlyInterestRate;
       }
-      
-      assetValue.value = currentValue;
-    };
-    
+
+      return currentValue;
+    }
+
+    watch([investmentPeriod, monthlyInvestment, annualInterestRate], () => {
+      assetValue.value = calculateAssetValue();
+    });
+
     return {
       investmentPeriod,
       monthlyInvestment,
       annualInterestRate,
       assetValue,
-      calculateAssetValue,
     };
   },
 });
@@ -48,11 +51,23 @@ export default defineComponent({
       <label>年利率（%）:</label>
       <input v-model="annualInterestRate" type="number" min="0" />
     </div>
-    <div>
-      <button @click="calculateAssetValue">計算する</button>
-    </div>
-    <div v-if="assetValue !== null">
+    <div class="export" v-if="assetValue !== null">
+      <p>
+        元本:
+        {{
+          Math.round(monthlyInvestment * investmentPeriod * 12).toLocaleString()
+        }}
+        円
+      </p>
       <p>将来の資産価値: {{ Math.round(assetValue).toLocaleString() }} 円</p>
+      <p>
+        増加額: +{{
+          Math.round(
+            assetValue - monthlyInvestment * investmentPeriod * 12
+          ).toLocaleString()
+        }}
+        円
+      </p>
     </div>
   </div>
 </template>
