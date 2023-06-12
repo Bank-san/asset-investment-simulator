a72656f1a2dffabb7293af108fab185e81b76f2d
bank-san <taichi.a@itnav.co.jp>
2023-06-12 09:02:44 +0900

    デザイン修正

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,41 @@
-<script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
+<script setup lang="ts">
+import { ref, watch } from 'vue';
 
-export default defineComponent({
-  name: 'AssetSimulator',
-  setup() {
-    const investmentPeriod = ref(10);
-    const monthlyInvestment = ref(10000);
-    const annualInterestRate = ref(5);
-    const assetValue = ref(calculateAssetValue());
+const investmentPeriod = ref(10);
+const monthlyInvestment = ref(10000);
+const annualInterestRate = ref(5);
+const assetValue = ref(calculateAssetValue());
 
-    function calculateAssetValue() {
-      const totalMonths = investmentPeriod.value * 12;
-      const monthlyInterestRate = annualInterestRate.value / 100 / 12;
+function calculateAssetValue() {
+  const totalMonths = investmentPeriod.value * 12;
+  const monthlyInterestRate = annualInterestRate.value / 100 / 12;
 
-      let currentValue = 0;
-      for (let i = 0; i < totalMonths; i++) {
-        currentValue += monthlyInvestment.value;
-        currentValue *= 1 + monthlyInterestRate;
-      }
+  let currentValue = 0;
+  for (let i = 0; i < totalMonths; i++) {
+    currentValue += monthlyInvestment.value;
+    currentValue *= 1 + monthlyInterestRate;
+  }
 
-      return currentValue;
-    }
+  return currentValue;
+}
 
-    watch([investmentPeriod, monthlyInvestment, annualInterestRate], () => {
-      assetValue.value = calculateAssetValue();
-    });
-
-    return {
-      investmentPeriod,
-      monthlyInvestment,
-      annualInterestRate,
-      assetValue,
-    };
-  },
+watch([investmentPeriod, monthlyInvestment, annualInterestRate], () => {
+  assetValue.value = calculateAssetValue();
 });
 </script>
 
 <template>
-  <div>
+  <div class="asset-simulator">
     <h2>資産運用シミュレーター</h2>
-    <div>
+    <div class="input-group">
       <label>投資期間（年）:</label>
       <input v-model="investmentPeriod" type="number" min="1" />
     </div>
-    <div>
+    <div class="input-group">
       <label>毎月の投資金額（円）:</label>
       <input v-model="monthlyInvestment" type="number" min="0" />
     </div>
-    <div>
+    <div class="input-group">
       <label>年利率（%）:</label>
       <input v-model="annualInterestRate" type="number" min="0" />
     </div>
@@ -69,5 +57,70 @@ export default defineComponent({
         円
       </p>
     </div>
+    <button class="calculate-button" @click="calculateAssetValue">
+      計算する
+    </button>
   </div>
 </template>
+
+<style scoped>
+.asset-simulator {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f5f5f5;
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.input-group {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.input-group label {
+  flex: 0 0 150px;
+  font-weight: bold;
+}
+
+.input-group input {
+  flex: 1;
+  padding: 6px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.export {
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+}
+
+.calculate-button {
+  display: block;
+  width: 100%;
+  padding: 8px;
+  border: none;
+  border-radius: 4px;
+  background-color: #4caf50;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.calculate-button:hover {
+  background-color: #45a049;
+}
+
+p {
+  margin: 10px 0;
+}
+</style>
